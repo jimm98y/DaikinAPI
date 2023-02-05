5e955b47970e81651c5fb889ed5a3d93ea9a8ae8
Lukas Volf <jimm98y@hotmail.com>
2023-02-05 15:30:15 +0100

    B Fixed deserialization of the JSON from Arduino

--- src/DaikinAPI/API/ReceivedDaikinState.cs
+++ src/DaikinAPI/API/ReceivedDaikinState.cs
@@ -2,24 +2,34 @@
 {
     public class ReceivedDaikinState
     {
+        [System.Text.Json.Serialization.JsonPropertyName("timestamp")]
         public int Timestamp { get; set; }
 
+        [System.Text.Json.Serialization.JsonPropertyName("temperature")]
         public int Temperature { get; set; }
 
+        [System.Text.Json.Serialization.JsonPropertyName("fan")]
         public int Fan { get; set; }
 
+        [System.Text.Json.Serialization.JsonPropertyName("powerState")]
         public int PowerState { get; set; }
 
+        [System.Text.Json.Serialization.JsonPropertyName("swing")]
         public int Swing { get; set; }
 
+        [System.Text.Json.Serialization.JsonPropertyName("timerOn")]
         public int TimerOn { get; set; }
 
+        [System.Text.Json.Serialization.JsonPropertyName("timerOnValue")]
         public int TimerOnValue { get; set; }
 
+        [System.Text.Json.Serialization.JsonPropertyName("timerOff")]
         public int TimerOff { get; set; }
 
+        [System.Text.Json.Serialization.JsonPropertyName("timerOffValue")]
         public int TimerOffValue { get; set; }
 
+        [System.Text.Json.Serialization.JsonPropertyName("mode")]
         public int Mode { get; set; }
     }
 }
